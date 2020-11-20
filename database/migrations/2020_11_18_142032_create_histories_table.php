06f57726ab46048a3458304bd827d5050f53bec3
Hideki Tanaka <tanaka2045@tbz.t-com.ne.jp>
2020-11-20 04:21:19 +0000

    PHP/Laravel 19課題

--- database/migrations/2020_11_18_142032_create_histories_table.php
+++ database/migrations/2020_11_18_142032_create_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfilehistoriesTable extends Migration
+class CreateHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateProfilehistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('profilehistories', function (Blueprint $table) {
+        Schema::create('histories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('profile_id');
+            $table->integer('news_id');
             $table->string('edited_at');
-            
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateProfilehistoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profilehistories');
+        Schema::dropIfExists('histories');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('profile/edit', 'Admin\ProfileController@edit');
   Route::post('profile/edit', 'Admin\ProfileController@update');
   
+  Route::get('/', 'NewsController@index');
+  
+  Route::get('/profile', 'ProfileController@index');
 
 });
 
