b77d3fc8acf084680615c72d2ef32f0ce58100b8
Hideki Tanaka <tanaka2045@tbz.t-com.ne.jp>
2020-11-22 01:09:56 +0000

    PHP/Laravel Heroku

--- database/migrations/2020_10_24_013754_create_news_table.php
+++ database/migrations/2020_10_24_013754_create_news_table.php
@@ -16,7 +16,7 @@ class CreateNewsTable extends Migration
         Schema::create('news', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title'); // ニュースのタイトルを保存するカラム
-            $table->string('body'); //ニュースの本文を保存するカラム
+            $table->text('body', 65535); //ニュースの本文を保存するカラム
             $table->string('image_path')->nullable(); //画像のパスを保存するカラム
             $table->timestamps();
         });
